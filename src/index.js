bd2252b1016c75e7ae168c81c2063e3dcdf4cb36
StephanieR20 <67565105+StephanieR20@users.noreply.github.com>
2021-04-13 03:17:51 -0400

    added icons, change the display
    upload weather icons for the app. I keep working on the search engine on JS.

--- src/index.js
+++ src/index.js
@@ -35,29 +35,13 @@ let timeElement = document.querySelector("#time");
 let currentHour = new Date();
 timeElement.innerHTML = formatTime(currentHour);
 
-//////////
+function getForecast(coordinates) {
+  let apiKey = "e8354e1f3a17775f04c6aee104fac2d4";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+  
+}
 
-/////////
-
-//function convertToFahrenheit(event) {
-//event.preventDefault();
-//let temperatureElement = document.querySelector("#temperature");
-//temperatureElement.innerHTML = convertToFahrenheit(event);
-//}
-
-//function convertToCelsius(event) {
-//event.preventDefault();
-//let temperatureElement = document.querySelector("#temperature");
-// temperatureElement.innerHTML = convertToCelsius(event);
-//}
-
-//let fahrenheitLink = document.querySelector("#fahrenheit-link");
-//fahrenheitLink.addEventListener("click", convertToFahrenheit);
-
-//let celsiusLink = document.querySelector("#celsius-link");
-//celsiusLink.addEventListener("click", convertToCelsius);
-
-///////
 
 function searchCity(event) {
   let apiKey = "e8354e1f3a17775f04c6aee104fac2d4";
@@ -65,12 +49,15 @@ function searchCity(event) {
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
-
 function handleSubmit(event) {
   event.preventDefault();
   let city = document.querySelector("#search-text-input").value;
   searchCity(city);
 }
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
 
 function searchLocation(position) {
   let apiKey = "e8354e1f3a17775f04c6aee104fac2d4";
@@ -78,16 +65,6 @@ function searchLocation(position) {
   axios.get(apiUrl).then(displayWeatherCondition);
 }
 
-function displayCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", handleSubmit);
-
-let currentLocationButton = document.querySelector("#current-location");
-currentLocationButton.addEventListener("click", displayCurrentLocation);
 
 function displayWeatherCondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
@@ -100,6 +77,11 @@ function displayWeatherCondition(response) {
   );
   document.querySelector("#description").innerHTML =
     response.data.weather[0].main;
-}
 
-searchCity("Washington DC");
+   let iconElement= document.querySelector("#icon");
+   iconElement.setAttribute(`icons`);
+    
+}
+searchCity("Washington");
+
+
