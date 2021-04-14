a413194234c6e0eda0bae5500dd8c0a10b4672ef
StephanieR20 <67565105+StephanieR20@users.noreply.github.com>
2021-04-14 14:32:06 -0400

    add a class to F
    there is still a bug on temperature display. need to figure.

--- src/index.js
+++ src/index.js
@@ -89,7 +89,7 @@ function displayFahrenheitTemperature (event){
 
  celsiusLink.classList.remove("active");
  fahrenheitLink.classList.add("active");
- let fahrenheitTemperature=(celsiusTemperature * 9) /5 + 32;
+ let fahrenheitTemperature=(celsiusTemperature * 9) / 5 + 32;
  temperatureElement.innerHTML= Math.round(fahrenheitTemperature);
 
  
